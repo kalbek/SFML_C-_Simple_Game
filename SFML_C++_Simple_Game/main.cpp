ccd2fc98a92ee0e4cb1ee01a243745db1efaf2e6
kaleb <access2kaleb@gmail.com>
2020-11-07 19:36:51 +0300

    Add project files.

---
+++ SFML_C++_Simple_Game/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <SFML/Audio.hpp>
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Network.hpp>
+using namespace std;
+int main()
+{ 
+    cout << "hello from SFML";
+    return 0;
+}
